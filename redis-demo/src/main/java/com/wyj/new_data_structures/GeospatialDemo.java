57f9b2ce214325ca19c4c2e95aa4f501c6fdc1b9
wangyj <850208971@qq.com>
2020-05-07 23:06:47 +0800

    geo demo

--- redis-demo/src/main/java/com/wyj/new_data_structures/GeospatialDemo.java
+++ redis-demo/src/main/java/com/wyj/new_data_structures/GeospatialDemo.java
@@ -121,4 +121,52 @@ public class GeospatialDemo {
          * 关闭客户端连接...
          */
     }
+
+
+
+
+    /**
+     * 找出位于指定元素周围的其他元素
+     * @author wangyj
+     * @Date 2020/5/7 10:50 下午
+     */
+    @Test
+    public void georadiusByMember(){
+        //取香河县（117.00634，39.76133）作为测试点,查看距离香河200公里以内的有哪些元素
+        myJedisClient.georadiusByMember("maincity", "beijing", 200, GeoUnit.KM)
+                .forEach(g-> System.out.println(g.getMemberByString()));
+
+
+        System.out.println("--------------------");
+        //如果是已经加入到地理空间的位置，可以直接用成员名进行检索
+        myJedisClient.georadiusByMember("maincity","beijing",200,GeoUnit.KM)
+                .forEach(g-> System.out.println(g.getMemberByString()));
+
+        /**
+         * console log
+         * 初始化客户端连接...
+         * 加入测试数据...
+         * tianjin
+         * beijing
+         * --------------------
+         * beijing
+         * tianjin
+         * 关闭客户端连接...
+         */
+    }
+
+    /**
+     * GeoHash 算法将二维的经纬度数据映射到一维的整数，这样所有的元素都将在挂载到一条线上，距离靠近的二维坐标映射到一维后的点之间距离也会很接近。
+     * 当我们想要计算「附近的人时」，首先将目标位置映射到这条线上，然后在这个一维的线上获取附近的点就行了
+     * @author wangyj
+     * @Date 2020/5/7 11:06 下午
+     */
+    @Test
+    public void geohash(){
+        System.out.println(myJedisClient.geohash("maincity","beijing", "tianjin", "shanghai"));
+        /**
+         * console log
+         * [wx4g0b7xrt0, wwgqddx7150, wtw3sjt9vg0]
+         */
+    }
 }
