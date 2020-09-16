588d22ae98dfb6d138d9b94cdddafb64b22b3427
wangyj <850208971@qq.com>
2020-09-16 09:27:58 +0800

    增加Integer先关

---
+++ my-snippet/src/main/java/com/wyj/IntegerSourceCode.java
@@ -0,0 +1,20 @@
+package com.wyj;
+
+import java.lang.reflect.Field;
+
+public class IntegerSourceCode {
+
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+
+        Class cache = Integer.class.getDeclaredClasses()[0]; //1
+        Field myCache = cache.getDeclaredField("cache"); //2
+        myCache.setAccessible(true);//3
+
+        Integer[] newCache = (Integer[]) myCache.get(cache); //4
+        newCache[132] = newCache[133]; //5
+
+        int a = 2;
+        int b = a + a;
+        System.out.printf("%d + %d = %d", a, a, b); //
+    }
+}
